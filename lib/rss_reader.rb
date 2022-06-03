83c378519ef2f09f62d0bff3c10250a99f3876a1
krakenchy <muja.pug@gmail.com>
2022-06-03 21:30:03 +0300

    Update rss_reader.rb

--- lib/rss_reader.rb
+++ lib/rss_reader.rb
@@ -3,6 +3,7 @@ require "rss"
 require "open-uri"
 
 # TESTING URL: https://kotaku.com/rss
+$urls = []
 
 class RSSReader
     #making url read only
@@ -44,21 +45,37 @@ class RSSReader
 
 end
 
-#Prompting user for url and asigning default value
-puts "Please enter the url from which you want the news..."
-get_url = gets.chomp
-if get_url == ""
-    get_url = "https://kotaku.com/rss"
-else
-    puts "URL gotten"
+def url
+    puts "Type the url you want to read news from..."
+    get_url = gets.chomp
+    if get_url == ""
+        get_url = "https://kotaku.com/rss"
+    else
+        puts "URL gotten"
+    end
+    reader = RSSReader.new(get_url)
+    reader.create_rss
+    puts reader.url
+    puts "----------------------"
+    $urls << get_url
+    puts "Urls that have been used so far: #{$urls}"
+    puts "----------------------"
 end
 
-#Creating instance of class and printing news onto terminal
-reader = RSSReader.new(get_url)
+def get_choice
+    puts "Would you like to try another url? y/n"
+    choice = gets.chomp.downcase
+case choice
+when "y"
+    url
+when "n"
+    puts "Exit404"
+end
+end
 
-reader.create_rss
+url
+get_choice
 
-#Printing all info and mentioning link at the bottom of the screen
-puts reader.url
+
 
 
